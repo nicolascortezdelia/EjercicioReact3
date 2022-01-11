26f8bb4769357507e595c9d236922ba044143e46
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-01-11 18:40:15 -0300

    agrego el state StateChange y el evento OnClick para que cuando presione el Button se muestre el texto from changed state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import { useState } from "react";
 import Titulo from "./components/Titulo"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
-  const [stateChange, setStateChange] = useState("(from changed state)")
+  
 
   return (
     <Titulo  hellomyfriend = "hello my friend!">
--- src/components/Titulo.js
+++ src/components/Titulo.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Form, Button } from 'react-bootstrap'; 
 
 
-const hellomyfriend = (props) => {
+const Titulo = (props) => {
+    const [stateChange, setStateChange] = useState("");
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        setStateChange("(from changed state)")
+        
+    }
+
+
+
     return (
         <div>
-            <h1>{props.hellomyfriend}</h1>
+            <h1>{props.hellomyfriend} {stateChange}</h1>
+            <Form onSubmit={handleSubmit}>
+  <Form.Group className="mb-3">
+    
+    
+    
+  </Form.Group>
+
+  <Button variant="primary" type="submit" onClick={(e)=>setStateChange()}>
+    Click Me
+  </Button>
+</Form>
         </div>
+
     );
 };
 
-export default hellomyfriend;
+export default Titulo;
