b21ceb02f542182f412d009d9f1725425ee62d10
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-01-11 14:09:42 -0300

    agrego bootstrap al proyecto

---
+++ src/components/Titulo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const hellomyfriend = (props) => {
+    return (
+        <div>
+            <h1>{props.hellomyfriend}</h1>
+        </div>
+    );
+};
+
+export default hellomyfriend;
